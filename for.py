4583333bb390302d87d2cb96dc398705c1fece9c
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-21 09:50:54 -0500

    Explorando un bucle diferente: el ciclo for

---
+++ for.py
@@ -0,0 +1,15 @@
+# contador = 1
+# print(contador)
+# while contador < 1000:
+#     contador += 1
+#     print(contador)
+
+
+# a = range(1000)
+# print(a)
+
+# for contador in range(1, 1001):
+#     print(contador)
+
+for i in range(10):
+    print(11 * i)
